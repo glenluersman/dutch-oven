7a737f6c9c341262bc5314d514e9d6ec40600d21
glenluersman <glen.luersman@gmail.com>
2022-03-25 20:02:15 -0400

    proficiencies

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,9 +4,39 @@ import resume from "../../assets/docs/Resume.pdf";
 function Resume() {
 
   return (
-    <div className="res">
-      <h2>Resume</h2>
-      <iframe title="resume" src={resume}></iframe>
+    <div>
+      <div className="res">
+        <h2>Resume</h2>
+      </div>
+      <div className="proficiency">
+        <div className="front">
+          <h4>Front-end Proficiencies</h4>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>jQuery</li>
+              <li>responsive design</li>
+              <li>React</li>
+              <li>Bootstrap</li>
+            </ul>
+        </div>
+        <div className="back">
+          <h4>Back-end Proficiencies</h4>
+            <ul>
+              <li>APIs</li>
+              <li>Node</li>
+              <li>Express</li>
+              <li>MySQL, Sequelize</li>
+              <li>MongoDB, Mongoose</li>
+              <li>REST</li>
+              <li>GraphQL</li>
+            </ul>
+        </div>    
+      </div>
+      <div className="doc">
+        <iframe title="resume" src={resume}></iframe>
+      </div>  
     </div>
   )
 }
